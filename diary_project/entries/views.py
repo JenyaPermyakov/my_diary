285a6d17b7bfb0bebc547fb4f08e9f458f074890
Yevgeniy <jekap99@mail.ru>
2026-04-27 11:07:10 +0500

    добавлен автор к задаче и реализован дизайн страницы ошибка 403

--- diary_project/entries/views.py
+++ diary_project/entries/views.py
@@ -2,23 +2,28 @@ from django.views.generic import ListView, DeleteView, UpdateView, CreateView, D
 from .models import Entry
 from .forms import EntryForm
 from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import redirect, get_object_or_404
 
+class AuthorRequiredMixin(UserPassesTestMixin):
+    def test_func(self):
+        obj = self.get_object()
+        return obj.user == self.request.user
 
-# 📌 Главная (НЕ показывает удалённые)
-class EntryListView(LoginRequiredMixin, ListView):
+class EntryListView(ListView):
     model = Entry
     template_name = "entries/entry_list.html"
     context_object_name = "entries"
-    ordering = "-id"
     paginate_by = 4
 
+    def get_ordering(self):
+        sort = self.request.GET.get('sort', '-id')
+        allowed = ['id', '-id', 'title', '-title', 'status']
+        return sort if sort in allowed else '-id'
+
     def get_queryset(self):
-        return Entry.objects.filter(user=self.request.user, is_deleted=False)
+        return Entry.objects.filter(is_deleted=False)
 
-
-# 📌 Создание
 class EntryCreateView(LoginRequiredMixin, CreateView):
     model = Entry
     form_class = EntryForm
@@ -29,15 +34,30 @@ class EntryCreateView(LoginRequiredMixin, CreateView):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
+class EntryDetailView(DetailView):
+    model = Entry
+    template_name = "entries/entry_detail.html"
+    context_object_name = "entry"
 
-# 📌 МЯГКОЕ УДАЛЕНИЕ
-class EntryDeleteView(LoginRequiredMixin, DeleteView):
+    def get_queryset(self):
+        return Entry.objects.filter(is_deleted=False)
+
+class EntryUpdateView(LoginRequiredMixin, AuthorRequiredMixin, UpdateView):
+    model = Entry
+    form_class = EntryForm
+    template_name = "entries/entry_update.html"
+    success_url = reverse_lazy("entry_list")
+
+    def get_queryset(self):
+        return Entry.objects.filter(is_deleted=False)
+
+class EntryDeleteView(LoginRequiredMixin, AuthorRequiredMixin, DeleteView):
     model = Entry
     template_name = "entries/entry_delete.html"
     success_url = reverse_lazy("entry_list")
 
     def get_queryset(self):
-        return Entry.objects.filter(user=self.request.user, is_deleted=False)
+        return Entry.objects.filter(is_deleted=False)
 
     def delete(self, request, *args, **kwargs):
         obj = self.get_object()
@@ -45,29 +65,6 @@ class EntryDeleteView(LoginRequiredMixin, DeleteView):
         obj.save()
         return redirect(self.success_url)
 
-
-# 📌 Редактирование
-class EntryUpdateView(LoginRequiredMixin, UpdateView):
-    model = Entry
-    form_class = EntryForm
-    template_name = "entries/entry_update.html"
-    success_url = reverse_lazy("entry_list")
-
-    def get_queryset(self):
-        return Entry.objects.filter(user=self.request.user, is_deleted=False)
-
-
-# 📌 Детали
-class EntryDetailView(LoginRequiredMixin, DetailView):
-    model = Entry
-    template_name = "entries/entry_detail.html"
-    context_object_name = "entry"
-
-    def get_queryset(self):
-        return Entry.objects.filter(user=self.request.user, is_deleted=False)
-
-
-# 📌 КОРЗИНА
 class DeletedEntryListView(LoginRequiredMixin, ListView):
     model = Entry
     template_name = "entries/deleted_entries.html"
@@ -76,17 +73,13 @@ class DeletedEntryListView(LoginRequiredMixin, ListView):
     def get_queryset(self):
         return Entry.objects.filter(user=self.request.user, is_deleted=True)
 
-
-# 📌 ВОССТАНОВЛЕНИЕ
 def restore_entry(request, pk):
     entry = get_object_or_404(Entry, pk=pk, user=request.user)
     entry.is_deleted = False
     entry.save()
     return redirect('deleted_entries')
 
-
-# 📌 УДАЛИТЬ НАВСЕГДА
 def hard_delete_entry(request, pk):
     entry = get_object_or_404(Entry, pk=pk, user=request.user)
-    Entry.objects.filter(pk=entry.pk).delete()
+    entry.delete()
     return redirect('deleted_entries')
